5652b8737fb79b524afcd984fa1a7f06feebc0de
Phil Bain <bain.pmcg@gmail.com>
2021-03-18 11:47:05 -0600

    add content security policy

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import App from 'next/app';
+import Head from 'next/head';
 import MakerProvider from '../providers/MakerProvider';
 import { ThemeProvider, Styled, Box } from 'theme-ui';
 import theme from '../theme';
@@ -46,6 +47,26 @@ class MyApp extends App {
     const { network } = this.state;
     return (
       <ThemeProvider theme={theme}>
+        <Head>
+          <meta
+            httpEquiv="Content-Security-Policy"
+            content={
+              "default-src 'none';" +
+              'frame-src https://connect.trezor.io;' +
+              "font-src 'self';" +
+              "connect-src 'self' https: wss:;" +
+              "style-src 'self' 'unsafe-inline';" +
+              `script-src 'self' ${
+                process.env.NODE_ENV === 'development' ? "'unsafe-eval'" : ''
+              };` +
+              "img-src 'self' https: data:"
+            }
+          />
+          <meta
+            name="description"
+            content="MakerDAO stakeholders use the Voting Portal to vote on the blockchain. Voting occurs frequently, requiring an active, well-informed governance community."
+          />
+        </Head>
         <Styled.root>
           <MakerProvider network={network}>
             <Header />
